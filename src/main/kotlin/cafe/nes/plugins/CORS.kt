c5b5e75a3de1664c00140eb3378c0116c2c7d9fe
nesb01t <nesb01t@163.com>
2024-02-10 13:27:08 +0800

    update allow post

--- src/main/kotlin/cafe/nes/plugins/CORS.kt
+++ src/main/kotlin/cafe/nes/plugins/CORS.kt
@@ -1,10 +1,15 @@
 package cafe.nes.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureCORS() {
     install(CORS) {
         anyHost()
+        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Patch)
+        allowMethod(HttpMethod.Delete)
     }
 }
